bacc9f8a960a92e828872ca66eb788a7fe694f91
Harrington Joseph <h.josephv@gmail.com>
2012-08-05 12:36:01 -0700

    Practice 2 - pushbutton and reader control

---
+++ practice_002_pushbutton_LED/pushbotton_LED/pushbotton_LED.ino
@@ -0,0 +1,35 @@
+// Example 02: Turn on LED while the button is pressed
+
+const int LED = 13; // The pin for the LED
+const int BUTTON = 7; // The input pin where the
+                      // pushbutton is connected
+int val = 0;
+
+int old_val = 0;
+
+int state = 0;
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  pinMode(BUTTON, INPUT);
+}
+
+void loop() {
+  val = digitalRead(BUTTON); // Read input value and store it
+  // Sends the value of the button to the LED pin
+  if ((val == HIGH) && (old_val == LOW)) {
+    state = 1 - state;
+  }
+  
+  // We need this to keep the previous value
+  // due to the high speed of the Arduino
+  // processor
+  old_val = val;
+  
+  if (state == 1) {
+    digitalWrite(LED, HIGH);
+  }
+  else {
+    digitalWrite(LED, LOW);
+  }
+}
